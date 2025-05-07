1b528f4395ea98b50aebee4901b3e2dd81cf789f
Akshay00008 <Akshay.sharma@algo8.ai>
2025-05-07 14:40:58 +0530

    print statements

--- src/config_reader.py
+++ src/config_reader.py
@@ -4,8 +4,9 @@ import os
 def get_rtsp_url(status: str, view: str) -> str:
     config = configparser.ConfigParser()
     config_path = os.path.join(os.getcwd(), 'config', 'cam_config.ini')
+    print(config_path)
     config.read(config_path)
-
+    print("config")
     try:
         return config[status][view]
     except KeyError:
